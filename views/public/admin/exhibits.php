fe3bad864ea34b13c5daa1dfa7ecff97a1329a24
David McClure <david.mcclure@virginia.edu>
2012-01-08 10:30:32 -0500

    adds ini setting for webservice slug

--- views/public/admin/exhibits.php
+++ views/public/admin/exhibits.php
@@ -27,41 +27,4 @@ echo $this->partial('admin/_header.php', array(
         <?php echo $this->partial('admin/_logout.php', array('user' => $user)); ?>
     </div>
 
-    <div class="row">
-
-        <div class="span13">
-
-            <table id="exhibits-table">
-                <thead>
-                    <tr>
-                        <th>exhibit</th>
-                        <th># items</th>
-                        <th>public</th>
-                    </tr>
-                </thead>
-                <?php foreach ($exhibits as $exhibit): ?>
-                    <tr>
-                        <td>
-                            <a class="exhibit-title" href=""><?php echo $exhibit->getExhibit()->name; ?></a>
-                            <div class="exhibit-slug">/<?php echo $exhibit->slug; ?></div>
-                            <span class="action bold"><a href="">edit</a> |</span>
-                            <span class="action"><a href="">embed</a> |</span>
-                            <span class="action danger"><a href="">delete</a></span>
-                        </td>
-                        <td></td>
-                        <td><?php echo $exhibit->public ? 'yes' : 'no'; ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-
-        </div>
-
-        <div class="span3">
-            <a class="no-underline" href="add">
-                <button id="new-exhibit-button" class="btn large primary icon alternative arrowup">New Exhibit</button>
-            </a>
-        </div>
-
-    </div>
-
 </div>
