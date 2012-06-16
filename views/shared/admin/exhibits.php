2cf7859ab080eddbfaf14bb43a14b957b4b121ec
David McClure <david.mcclure@virginia.edu>
2012-06-16 12:52:47 -0400

    changes to browse templates

--- views/shared/admin/exhibits.php
+++ views/shared/admin/exhibits.php
@@ -42,9 +42,8 @@ echo $this->partial('admin/_header.php', array(
                 <thead>
                     <tr>
                         <th>exhibit</th>
-                        <th>slug</th>
                         <th>modified</th>
-                        <th># items</th>
+                        <th>items</th>
                         <th>public</th>
                         <th>edit</th>
                     </tr>
@@ -53,16 +52,16 @@ echo $this->partial('admin/_header.php', array(
                     <tr>
                         <td>
                             <a class="exhibit-title" href="<?php echo nlws_show_url($exhibit->slug); ?>"><?php echo $exhibit->getExhibit()->name; ?></a>
+                            <div class="exhibit-slug">/<?php echo $exhibit->slug; ?></div>
                             <span class="action bold"><a href="<?php echo nlws_url('edit', $exhibit->slug); ?>">edit details</a> |</span>
                             <span class="action danger"><a href="<?php echo nlws_url('delete', $exhibit->slug); ?>">delete</a></span>
                         </td>
-                        <td><div class="exhibit-slug">/<?php echo $exhibit->slug; ?></div></td>
                         <td><?php echo nlws_formatDate($exhibit->modified); ?></td>
                         <td><?php echo $exhibit->getNumberOfRecords(); ?></td>
                         <td><?php echo $exhibit->public ? 'yes' : 'no'; ?></td>
                         <td>
                             <a class="no-underline" href="<?php echo nlws_url('editor', $exhibit->slug); ?>">
-                                <button id="edit-exhibit-button" class="btn large primary icon alternative">Edit</button>
+                                <button id="edit-exhibit-button" class="btn primary icon alternative">Edit</button>
                             </a>
                         </td>
                     </tr>
